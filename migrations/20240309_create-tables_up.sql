45eb1378810bb7ba961ece119b91c9c4e8a43bcf
damir <damirem2004@mail.ru>
2024-03-14 21:10:37 +0300

    Дз4

--- migrations/20240309_create-tables_up.sql
+++ migrations/20240309_create-tables_up.sql
@@ -1,8 +1,9 @@
 -- Файл миграции: создание таблицы ссылок и таблицы чатов
 create table link
 (
-    link_id         bigint generated always as identity,
-    url             varchar(255)              not null,
+    link_id    bigint generated always as identity not null,
+    url        varchar(255)                        not null,
+    last_check timestamp with time zone,
 
     primary key (link_id),
     unique (url)
@@ -10,7 +11,7 @@ create table link
 
 create table chat
 (
-    chat_id         bigint generated always as identity,
+    chat_id bigint not null,
 
     primary key (chat_id)
 );
@@ -18,10 +19,10 @@ create table chat
 -- Связывающая таблица для отслеживания связей между ссылками и чатами
 create table link_chat_mapping
 (
-    chat_id         bigint                    not null,
-    link_id         bigint                    not null,
+    chat_id bigint not null,
+    link_id bigint not null,
 
     primary key (chat_id, link_id),
-    foreign key (link_id) references link(link_id),
-    foreign key (chat_id) references chat(chat_id)
+    foreign key (link_id) references link (link_id),
+    foreign key (chat_id) references chat (chat_id)
 );
